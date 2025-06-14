8562b908dab2be0d3ba65320f382861f74c61664
Tony Kwong <tonykkn4321@gmail.com>
2025-06-14 10:18:09 +0800

    8th commit: Using DRF generic views to generate generic PollList, PollDetail, ChoiceLIst & CreateVote api views

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .apiviews import PollList, PollDetail
+from .apiviews import ChoiceList, CreateVote
 
 urlpatterns = [
     path("polls/", PollList.as_view(), name="polls_list"),
-    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail")
+    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+    path("choices/", ChoiceList.as_view(), name="choice_list"),
+    path("vote/", CreateVote.as_view(), name="create_vote"),    
 ]
