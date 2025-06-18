700b228ebba36d47814d1fb512b2380945e793f4
Tony Kwong <tonykkn4321@gmail.com>
2025-06-18 09:52:03 +0800

    20th commit: Adding swagger documentation

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .apiviews import PollViewSet, ChoiceList, CreateVote, UserCreate, LoginView
 from rest_framework.routers import DefaultRouter
+from .swagger import schema_view
+
 
 
 router = DefaultRouter()
@@ -11,6 +13,7 @@ urlpatterns = [
     path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
     path("users/", UserCreate.as_view(), name="user_create"),  
     path("login/", LoginView.as_view(), name="login"),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 urlpatterns += router.urls
--- pollsapi/settings.py
+++ pollsapi/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'polls',
+    'drf_yasg',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
