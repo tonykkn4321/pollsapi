e2f0a36f3337da8daa2620a68eb4020c77d2b7f5
Tony Kwong <tonykkn4321@gmail.com>
2025-08-13 09:32:35 +0800

    Modified setting.py and added railway.json for deploying to Railway

--- pollsapi/settings.py
+++ pollsapi/settings.py
@@ -14,6 +14,7 @@ import sys
 import json
 import base64
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,11 +29,7 @@ SECRET_KEY = 'django-insecure-(&p#lyns6f3w)7-8-lb7&o6mwfbdzhr&6de#%pb!l^&ja02&(i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '.platformsh.site',
-]
+ALLOWED_HOSTS = ['*']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
@@ -93,9 +90,11 @@ WSGI_APPLICATION = 'pollsapi.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL")),
+    
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -130,65 +129,15 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#################################################################################
-# Platform.sh-specific configuration
-
-# This variable should always match the primary database relationship name,
-#   configured in .platform.app.yaml.
-PLATFORMSH_DB_RELATIONSHIP="database"
-
-# Helper function for decoding base64-encoded JSON variables.
-def decode(variable):
-    """Decodes a Platform.sh environment variable.
-    Args:
-        variable (string):
-            Base64-encoded JSON (the content of an environment variable).
-    Returns:
-        An dict (if representing a JSON object), or a scalar type.
-    Raises:
-        JSON decoding error.
-    """
-    try:
-        if sys.version_info[1] > 5:
-            return json.loads(base64.b64decode(variable))
-        else:
-            return json.loads(base64.b64decode(variable).decode('utf-8'))
-    except json.decoder.JSONDecodeError:
-        print('Error decoding JSON, code %d', json.decoder.JSONDecodeError)
-
-# Import some Platform.sh settings from the environment.
-if (os.getenv('PLATFORM_APPLICATION_NAME') is not None):
-    DEBUG = False
-    if (os.getenv('PLATFORM_APP_DIR') is not None):
-        STATIC_ROOT = os.path.join(os.getenv('PLATFORM_APP_DIR'), 'static')
-    if (os.getenv('PLATFORM_PROJECT_ENTROPY') is not None):
-        SECRET_KEY = os.getenv('PLATFORM_PROJECT_ENTROPY')
-    # Database service configuration, post-build only.
-    if (os.getenv('PLATFORM_ENVIRONMENT') is not None):
-        platformRelationships = decode(os.getenv('PLATFORM_RELATIONSHIPS'))
-        db_settings = platformRelationships[PLATFORMSH_DB_RELATIONSHIP][0]
-        DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.postgresql',
-                'NAME': db_settings['path'],
-                'USER': db_settings['username'],
-                'PASSWORD': db_settings['password'],
-                'HOST': db_settings['host'],
-                'PORT': db_settings['port'],
-            },
-            'sqlite': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            }
-        }
