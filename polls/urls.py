b313da08a54afa79fb467b8e9f29bc1f906b9795
Tony Kwong <tonykkn4321@gmail.com>
2025-10-02 14:25:32 +0800

    Using DRF generic views to simplify code

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from .views import polls_list, polls_detail
+# For views.py:
+# from .views import polls_list, polls_detail
+from .apiviews import PollList, PollDetail
 
+# For views.py:
+'''
 urlpatterns = [
     path("polls/", polls_list, name="polls_list"),
     path("polls/<int:pk>/",polls_detail,name="polls_detail")
+]
+'''
+# For apiviews.py:
+urlpatterns = [
+    path("polls/", PollList.as_view(), name="polls_list"),
+    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail")
 ]
