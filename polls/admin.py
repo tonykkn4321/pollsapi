b305c1406272dfb9f9b5ae52c473f173be3c5a7c
Tony Kwong <tonykkn4321@gmail.com>
2025-10-02 13:33:35 +0800

    Initial commit for version_2

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
 
-from .models import Poll, Choice
-admin.site.register(Poll)
-admin.site.register(Choice)
+# Register your models here.
--- pollsapi/urls.py
+++ pollsapi/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^', include('polls.urls')),
 ]
